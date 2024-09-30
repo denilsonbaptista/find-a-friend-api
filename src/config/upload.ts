fcf2fd46deea6895aa1b43818efab28d9be38bd7
denilson <baptistadenilsonbp@gmail.com>
2024-09-30 19:16:08 -0300

    chore: add config and providers directories

---
+++ src/config/upload.ts
@@ -0,0 +1,18 @@
+import crypto from 'node:crypto'
+import path from 'node:path'
+import multer from 'fastify-multer'
+
+export const TMP_FOLDER = path.resolve(__dirname, '..', '..', 'tmp')
+export const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, 'uploads')
+
+export const MULTER = {
+  storage: multer.diskStorage({
+    destination: TMP_FOLDER,
+    filename(request, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString('hex')
+      const fileName = `${fileHash}-${file.originalname}`
+
+      return callback(null, fileName)
+    },
+  }),
+}
