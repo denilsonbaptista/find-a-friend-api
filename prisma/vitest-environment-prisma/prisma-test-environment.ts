6e28079685773152af5e09f519101c9c418ea7c2
denilson <baptistadenilsonbp@gmail.com>
2024-10-05 00:58:00 -0300

    chore: add script to lint

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,6 +1,6 @@
-import { PrismaClient } from '@prisma/client'
 import { execSync } from 'node:child_process'
 import { randomUUID } from 'node:crypto'
+import { PrismaClient } from '@prisma/client'
 
 import type { Environment } from 'vitest/environments'
 
